8782e3dcf34da9f6ef30744d1f7cf535f1893846
DavidForero120 <DavidForero120>
2022-10-19 23:28:04 -0500

    cv1 estile

---
+++ js/eventos.js
@@ -0,0 +1,6 @@
+const bot1 = document.getElementById('butt1');
+
+bot1.addEventListener('click', ()=>{
+      const about = document.getElementById('about');
+      about.classList.toggle('active');
+})
