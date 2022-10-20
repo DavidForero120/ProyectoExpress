a8d551293b1b2c8444e29a474d57b8326fea417b
Your Name <you@example.com>
2022-10-20 12:05:26 -0500

    express rutas

--- ejercicio2.js
+++ ejercicio2.js
@@ -4,13 +4,30 @@ const Express = require('express');
 const ejercicio = Express();
 const port = 3002;
 
-ejercicio.get('/cliente', (req, res)=>{
-      res.send("Ruta express, reto introduccion");
+ejercicio.use(Express.static(__dirname + '/public'));
+
+ejercicio.set('view engine', 'ejs');
+
+ejercicio.set('views', __dirname + '/views');
+
+
+ejercicio.get('/perfil', (req, res)=>{
+      res.render('index', {nombre: "David Orlando Forero Moreno"});    
 });
 
-ejercicio.get('/', (req, res)=>{
-      res.send("Servidor");
+
+ejercicio.get('/user', (req, res)=>{
+      res.send("route user");
 });
+
 ejercicio.listen(port, ()=>{
       console.log(`servidor inicializado en el puerto ${port}`);
+      
+});
+
+
+ejercicio.use((req , res, next)=>{
+      res.status(404).sendFile(__dirname + '/public/404.html')
 })
+
+
