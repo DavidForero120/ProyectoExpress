ffe44c40ba5b68b4a004d09f69432d23496b9a98
DavidForero120 <DavidForero120>
2022-10-22 10:13:28 -0500

    cv, error

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,6 +1,27 @@
 const bot1 = document.getElementById('butt1');
+const bot2 = document.getElementById('butt2');
+const bot3 = document.getElementById('butt4');
+
+/* parts*/
+const about = document.getElementById('about');
+const resum = document.getElementById('resume_');
+const cont = document.getElementById('contact_');
+
 
 bot1.addEventListener('click', ()=>{
-      const about = document.getElementById('about');
-      about.classList.toggle('active');
+      about.classList.toggle('active');  
+      resum.classList.replace('res', 'resume_');
+      cont.classList.replace('cont_off','contact_');
+});
+
+bot2.addEventListener('click', ()=>{
+      resum.classList.toggle('res');   
+      about.classList.replace('active', 'about');
+      cont.classList.replace('cont_off','contact_');
+});
+
+bot3.addEventListener('click', ()=>{
+      cont.classList.toggle('cont_off');
+      about.classList.replace('active', 'about');
+      resum.classList.replace('res', 'resume_');
 })
